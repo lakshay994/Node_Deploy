e0eab83124b90ca48eee45e89fec4bf4169f63a4
Lakshay Sharma <lakshay.sharma994@gmail.com>
2018-09-04 07:10:51 -0500

    Initial Commit

---
+++ server.js
@@ -0,0 +1,55 @@
+console.log('Starting server.js....');
+
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+app.set('view engine', hbs);
+
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getCurrentyear', () => {
+    return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+// custom express middleware
+app.use((req, res, next) => {
+
+    let requestInfo = new Date().toString();
+
+    fs.appendFile('server.log', requestInfo + '\n', (err) => {
+        if(err){
+            console.log('Could not write to file');
+        }
+    });
+    next();
+});
+
+// app.use((req, res, next) => {
+//     res.render('maintainence.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));  // middleware to serve the public directory
+
+app.get('/', (req, res) => {
+    res.render('index.hbs', {
+        heading: 'Hello Express!',
+        year: new Date().getFullYear()
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        heading: 'Hello Express! About:',
+        year: new Date().getFullYear()
+    });
+});
+
+app.listen(port, () => {
+    console.log('Server listening at port 3000');
+});
